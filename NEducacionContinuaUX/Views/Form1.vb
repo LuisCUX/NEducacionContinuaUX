97f4ec8deefbd60ae0e63b8915cddec40fd3f94b
chupi <wea>
2022-05-11 10:50:35 -0500

    Cancelacionv4

---
+++ NEducacionContinuaUX/Views/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Se instancia el WS de Timbrado.
+        Dim ServicioTimbrado_FEL As New TimbradoUXReal.WSCFDI33Client
+
+        'Se instancia la Respuesta del WS de Timbrado.
+        Dim RespuestaServicio_FEL As New TimbradoUXReal.RespuestaTFD33
+
+        'Se realiza la petición al WebService, almacenando la respuesta en el objeto RespuestaTFD (RespuestaServicio_FEL)
+        'Los parametros son usuario,password,referencia
+        'Los datos de acceso se deben solicitar a FEL.
+        RespuestaServicio_FEL = ServicioTimbrado_FEL.ConsultarTimbrePorReferencia("ECU150924HR4", "JCXM5@uUgr+", TextBox1.Text)
+
+    End Sub
+End Class
